7a033df5a8a01338118c6db722872f339d20b9ca
yoannfb <yoannfb@hotmail.com>
2025-04-04 09:34:50 +0200

    Procfile for Heroku

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <h1>youpi 2</h1>
 
 <?php 
-include "../html/accueil.html";
+include "C:\Users\yoann\yoann\Studi\Angular\lamagit\html\accueil.html";
 include "../css/accueil.css";
 include "../js/accueil.js";
 ?>
