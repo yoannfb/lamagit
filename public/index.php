5fa7b0577f8bc9d52f6df6e3f4b474529fb91b6a
yoannfb <yoannfb@hotmail.com>
2025-04-04 09:47:55 +0200

    Procfile for Heroku

--- public/index.php
+++ public/index.php
@@ -1,7 +1,7 @@
 <h1>youpi 2</h1>
 
 <?php 
-include "html\accueil.html";
-include "js\accueil.js";
-include "css\accueil.css";
+include "../html/accueil.html";
+include "../js/accueil.js";
+include "../css/accueil.css";
 ?>
