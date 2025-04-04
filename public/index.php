643c0a80c09533529d94cdec553a403c927a30f6
yoannfb <yoannfb@hotmail.com>
2025-04-04 10:23:12 +0200

    Procfile for Heroku

--- public/index.php
+++ public/index.php
@@ -1,6 +1,30 @@
-<h1>youpi 2</h1>
-
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/accueil.css">
+    <script src="https://kit.fontawesome.com/ac5aebefd7.js" crossorigin="anonymous"></script>
+    <title>L'Amagit</title>
+</head>
+<body>
 <?php 
 include "../html/accueil.html";
 
 ?>
+    <footer>
+        <div class="footer">
+            <p>&#169 L'Amagit</p>
+            <p>Suivez-nous</p>
+            <a href=""><i class="fa-brands fa-facebook"></i></a>
+            <a href=""><i class="fa-brands fa-square-x-twitter"></i></a>
+            <a href=""><i class="fa-brands fa-instagram"></i></a>
+            <a href="#">Mentions-legales</a>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.7/dist/gsap.min.js"></script>
+    <script src="../lamagit/js/accueil.js"></script>
+</body>
+</html>
+
